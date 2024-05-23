90345f697cc40f877b3b09871e1a80aa9583e60e
FunafutiTV <113341799+FunafutiTV@users.noreply.github.com>
2024-05-23 16:54:12 +0200

    changed portfolio url

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="footer">
             <span>Nathanael Kubski, 2024</span>
             <span><img src="/github.png"/><a href="https://github.com/FunafutiTV/" target="_blank">GitHub</a></span>
-            <span><img src="nk.png"/><a href="https://nathanaelkubski.vercel.app/" target="_blank">Contact & Info</a></span>
+            <span><img src="nk.png"/><a href="https://nathanaelkubski.me/" target="_blank">Contact & Info</a></span>
         </div>
     )
 }
